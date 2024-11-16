9ca7ee6179b3830f9c2e82c32453c0940b3aa3ec
Олег Ерофеев <lionbolshoe@yandex.ru>
2024-11-16 11:39:38 +0300

    Oleg commit 11 (+KNN +parser +event)

--- handlers/classification_functions/select_event_by_topic.py
+++ handlers/classification_functions/select_event_by_topic.py
@@ -1,20 +1,34 @@
 from telebot import types
 from bot_instance import bot
 
+# Глобальный словарь для хранения информации о событиях
 event_data = {}
 event_counter = 0  # Глобальный счётчик для уникальных идентификаторов событий
 
-def select_event(message, events_array):
-    # Функция предлагает пользователю выбрать событие из списка
 
+def select_event(message, events_array):
+    """
+    Эта функция предлагает пользователю выбрать событие из списка,
+    отображая название и дату каждого события.
+    """
     global event_counter
 
-    # Создаем сообщение с названиями событий
-    message_text = f"🎭 Яндекс Афиша: {events_array[0]['topic']}\n\n"
-    for i, event_item in enumerate(events_array, start=1):
-        message_text += f"📅 Событие {i}: {event_item['title']}\n\n"
-    markup = types.InlineKeyboardMarkup()
+    if not events_array:
+        bot.send_message(message.chat.id, "На данный момент актуальных событий не найдено.")
+        return
 
+    # Начинаем формировать текст сообщения с темой
+    message_text = f"🎭 Яндекс Афиша: {events_array[0]['topic']}\n\n"
+
+    # Добавляем информацию о каждом событии: номер, название и дату
+    for i, event_item in enumerate(events_array, start=1):
+        message_text += (
+            f"📅 <b>Событие {i}:</b> {event_item['title']}\n"
+            f"🗓 <b>Дата:</b> {event_item['date']}\n\n"
+        )
+
+    # Создаём разметку для инлайн-кнопок
+    markup = types.InlineKeyboardMarkup()
     buttons = []
 
     for i, event_item in enumerate(events_array, start=1):
@@ -24,36 +38,52 @@ def select_event(message, events_array):
         event_data[str(event_id)] = event_item
         # В callback_data используем уникальный идентификатор события
         button = types.InlineKeyboardButton(
-            f"Событие {i}", callback_data=f'event_{event_id}'
+            f"Подробнее о событии {i}",
+            callback_data=f'event_{event_id}'
         )
         buttons.append(button)
 
-    # Добавляем кнопки в разметку
-    markup.row(buttons[0], buttons[1])
-    markup.row(buttons[2], buttons[3])
-    markup.row(buttons[4])
+    # Размещаем кнопки по 2 в ряд. Если количество кнопок нечётное, последняя будет одна.
+    for i in range(0, len(buttons), 2):
+        if i + 1 < len(buttons):
+            markup.add(buttons[i], buttons[i + 1])
+        else:
+            markup.add(buttons[i])
 
     # Отправляем сообщение с разметкой
     bot.send_message(
         message.chat.id,
         message_text,
+        parse_mode='HTML',  # Позволяет использовать HTML-теги для форматирования
         reply_markup=markup
     )
 
+
 @bot.callback_query_handler(func=lambda callback: callback.data.startswith('event_'))
 def callback_event(callback):
-    # Обработка нажатия на кнопку события
+    """
+    Обработка нажатия на кнопку события.
+    Отправляет пользователю подробную информацию о выбранном событии.
+    """
     event_id = callback.data.split('_')[1]
     event_item = event_data.get(event_id)
+
     if event_item:
-        # Отправляем ссылку на событие
+        # Формируем подробное сообщение о событии
+        detailed_message = (
+            f"🎭 <b>{event_item['title']}</b>\n"
+            f"🗓 <b>Дата:</b> {event_item['date']}\n"
+            f"🔗 <b>Ссылка на событие:</b> {event_item['link']}"
+        )
         bot.send_message(
             callback.from_user.id,
-            f"Ссылка на событие: {event_item['link']}",
+            detailed_message,
+            parse_mode='HTML',
+            disable_web_page_preview=True  # Отключает предпросмотр ссылок для чистого вида
         )
     else:
         # Сообщаем, что событие не найдено или ссылка устарела
         bot.send_message(
             callback.from_user.id,
-            "Событие не найдено или ссылка устарела. Попробуйте еще раз!",
+            "❌ Событие не найдено или ссылка устарела. Попробуйте еще раз!",
         )
--- models/parser/event_parser/event_parser_file.py
+++ models/parser/event_parser/event_parser_file.py
@@ -7,15 +7,16 @@ from handlers.classification_functions.select_event_by_topic import select_event
 def send_event_news(message):
     """
     Эта функция получает последние события с указанного URL и отправляет
-    массив из первых 5 событий вместе с их ссылками в функцию select_event().
+    массив из первых 5 событий вместе с их ссылками и датами в функцию select_event().
     """
     url = 'https://afisha.yandex.ru/moscow/selections/nearest-events'  # URL страницы с событиями
     base_url = 'https://afisha.yandex.ru'  # Базовый URL для формирования полных ссылок
 
     # Заголовки для имитации браузера
-    headers = {'accept': '*/*',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
-              }
+    headers = {
+        'User-Agent': urllib.parse.quote('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, как Gecko) '
+                      'Chrome/58.0.3029.110 Safari/537.3')
+    }
 
     # Отправляем GET-запрос на страницу
     response = requests.get(url, headers=headers)
@@ -29,24 +30,45 @@ def send_event_news(message):
         events = soup.find_all("div", class_="event events-list__item yandex-sans")
         events_url = soup.find_all("div", class_="Badges-njdnt8-7 eSHAvL")
 
-        print(events, events_url)
-
         # Проверяем, что найдены события
         if events and events_url:
             events_array = []
             # Извлекаем первые 5 событий
             for event, event_url in zip(events[:5], events_url[:5]):
+                # Извлечение названия события
                 name_tag = event.find("div", class_="Root-fq4hbj-4 iFrhLC").find('h2')
+                # Извлечение даты события
+                # Предполагается, что дата находится внутри тега <ul><li> внутри блока event
+                date_tag = event.find('ul')
+                if date_tag:
+                    date_text = date_tag.find('li').get_text(strip=True)
+                else:
+                    date_text = "Дата не указана"
+
+                # Извлечение ссылки на событие
                 link_tag = event_url.find('a')
                 if name_tag and link_tag:
                     name = name_tag.get_text(strip=True)
-                    link = link_tag['href']
-                    # Преобразуем относительную ссылку в абсолютную
-                    absolute_link = urllib.parse.urljoin(base_url, link)
-                    # Добавляем событие в массив
-                    events_array.append({'topic': 'События', 'title': name, 'link': absolute_link})
-            # Вызываем функцию select_event() с массивом событий
-            select_event(message, events_array)
+                    link = link_tag.get('href')
+                    if link:
+                        # Преобразуем относительную ссылку в абсолютную
+                        absolute_link = urllib.parse.urljoin(base_url, link)
+                    else:
+                        absolute_link = "Ссылка не указана"
+
+                    # Добавляем событие в массив с датой
+                    events_array.append({
+                        'topic': 'События',
+                        'title': name,
+                        'link': absolute_link,
+                        'date': date_text
+                    })
+
+            if events_array:
+                # Вызываем функцию select_event() с массивом событий
+                select_event(message, events_array)
+            else:
+                bot.send_message(message.chat.id, "На данный момент актуальных событий не найдено.")
         else:
             bot.send_message(message.chat.id, "На данный момент актуальных событий не найдено.")
     else:
